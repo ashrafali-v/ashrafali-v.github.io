63893a76f49cd4e6f0d8c1b2285a93fdd28e8996
ashrafali-v <ashruashrafali@gmail.com>
2020-04-24 10:52:33 +0530

    issue fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {APP_BASE_HREF} from '@angular/common';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ],
-  providers: [{provide: APP_BASE_HREF, useValue: '/dist/localization1/en-US'}]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }

5f23382499967a051f32049c76a8e4deab558dbb
ashrafali-v <ashruashrafali@gmail.com>
2020-04-24 11:44:57 +0530

    update with $localeId variable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  minutes = 0;
-  gender = 'female';
-  fly = true;
-  logo = 'https://angular.io/assets/images/logos/angular/angular.png';
-  inc(i: number) {
-    this.minutes = Math.min(5, Math.max(0, this.minutes + i));
-  }
-  male() { this.gender = 'male'; }
-  female() { this.gender = 'female'; }
-  other() { this.gender = 'other'; }
+  languages = [
+    { code: 'en', label: 'English'},
+    { code: 'es', label: 'Español'},
+    { code: 'fr', label: 'Français'}
+  ];
+  constructor(@Inject(LOCALE_ID) protected localeId: string) {}
 }

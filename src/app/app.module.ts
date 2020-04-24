60aad8eba069048eea00639d37d7da741e0a9dc7
ashrafali-v <ashruashrafali@gmail.com>
2020-04-24 09:51:52 +0530

    add dist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {APP_BASE_HREF} from '@angular/common';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  providers: [{provide: APP_BASE_HREF, useValue: '/dist/localization1/en-US'}]
 })
 export class AppModule { }
